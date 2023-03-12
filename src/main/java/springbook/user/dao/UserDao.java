eec1fa334ea07564bc6c58014b6c47f3ec4f95ea
mingadinga <minpearl0826@gmail.com>
2023-03-12 22:52:56 +0900

    3.6 JdbcTemplate 적용

--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -1,121 +1,121 @@
 package springbook.user.dao;
 
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import springbook.user.domain.User;
 
 import javax.sql.DataSource;
 import java.sql.*;
+import java.util.List;
 
 public class UserDao {
 
-    DataSource dataSource;
+//    DataSource dataSource;
+//
+//    public void setDataSource(DataSource dataSource) {
+//        this.dataSource = dataSource;
+//    }
+//
+//    JdbcContext jdbcContext;
+//
+//    public void setJdbcContext(JdbcContext jdbcContext) {
+//        this.jdbcContext = jdbcContext;
+//    }
+
+    private JdbcTemplate jdbcTemplate;
 
     public void setDataSource(DataSource dataSource) {
-        this.dataSource = dataSource;
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
-    JdbcContext jdbcContext;
-
-    public void setJdbcContext(JdbcContext jdbcContext) {
-        this.jdbcContext = jdbcContext;
-    }
-
+    // update()
+    // 반환값 x
+    // 치환자 o
     public void add(User user) throws SQLException {
-        StatementStrategy addStatementStrategy = c -> {
-            PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
-            ps.setString(1, user.getId());
-            ps.setString(2, user.getName());
-            ps.setString(3, user.getPassword());
-            return ps;
-        };
+        // 커스텀 전략 클래스
+//        StatementStrategy addStatementStrategy = c -> {
+//            PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
+//            ps.setString(1, user.getId());
+//            ps.setString(2, user.getName());
+//            ps.setString(3, user.getPassword());
+//            return ps;
+//        };
 
-        jdbcContext.workWithStatementStrategy(addStatementStrategy);
+        // PreparedStatement를 사용하는 콜백
+//        jdbcTemplate.update(con -> {
+//            PreparedStatement ps = con.prepareStatement("insert into users(id, name, password) values(?,?,?)");
+//            ps.setString(1, user.getId());
+//            ps.setString(2, user.getName());
+//            ps.setString(3, user.getPassword());
+//            return ps;
+//        });
+
+        // static sql을 사용하는 콜백
+        jdbcTemplate.update("insert into users(id, name, password) values(?,?,?)",
+                user.getId(), user.getName(), user.getPassword());
     }
 
+
+    // queryForObject(sql, parameters, RowMapper)
+    // 반환값 o
+    // 치환자 o
     public User get(String id) throws SQLException, EmptyResultDataAccessException {
-        Connection c = null;
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-
-        try {
-            c = dataSource.getConnection();
-
-            ps = c.prepareStatement("select * from users where id = ?");
-            ps.setString(1, id);
-
-            rs = ps.executeQuery();
-
-            User user = null;
-            if (rs.next()) {
-                user = new User();
-                user.setId(rs.getString("id"));
-                user.setName(rs.getString("name"));
-                user.setPassword(rs.getString("password"));
-            }
-
-            if (user == null) throw new EmptyResultDataAccessException(1);
-            return user;
-
-        } catch (SQLException e) {
-            throw e;
-        } finally {
-            if (ps != null) {
-                try {
-                    ps.close();
-                } catch (SQLException e) {}
-            }
-            if (c != null) {
-                try {
-                    c.close();
-                } catch (SQLException e) {}
-            }
-            if (rs != null) {
-                try {
-                    rs.close();
-                } catch (SQLException e) {}
-            }
-        }
-
-
+        return this.jdbcTemplate.queryForObject(
+                "select * from users where id = ?",
+                new Object[]{id},
+                getUserRowMapper()
+        );
     }
 
-    // 클라이이언트 : DI
+    // update()
+    // 반환값 x
+    // 치환자 x
     public void deleteAll() throws SQLException {
-        jdbcContext.executeSQL("delete from users");
+        // Methods dealing with prepared statements
+        // PreparedStatementCallback 사용
+//        jdbcTemplate.update(new PreparedStatementCreator() {
+//            @Override
+//            public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+//                return con.prepareStatement("delete from users");
+//            }
+//        });
+        // Methods dealing with static SQL (java.sql.Statement)
+        // StatementCallback 사용
+        jdbcTemplate.update("delete from users");
     }
 
+
+    // query(PreparedStatement, ResultSetExtractor<Integer>)
+    // 반환값 o
+    // 치환자 x
     public int getCount() throws SQLException {
-        Connection c = null;
-        PreparedStatement ps = null;
-        ResultSet rs = null;
+        // PreparedStatement 사용
+        // PreparedStatement, ResultSetExtractor<Integer> 넘겨줌
+        return this.jdbcTemplate.query(
+                con -> con.prepareStatement("select count(*) from users"),
+                rs1 -> { rs1.next(); return rs1.getInt(1); }
+        );
+    }
 
-        try {
-            c = dataSource.getConnection();
-            ps = c.prepareStatement("select count(*) from users");
-            rs = ps.executeQuery();
-            rs.next();
-            return rs.getInt(1);
-        } catch (SQLException e) {
-            throw e;
-        } finally {
-            if (rs != null) {
-                try {
-                    rs.close();
-                } catch (SQLException e) {}
-            }
+    // query(sql, RowMapper)
+    // 반환값 o
+    // 치환자 x
+    public List<User> getAll() {
+        return this.jdbcTemplate.query(
+                "select * from users order by id",
+                getUserRowMapper()
+        );
 
-            if (ps != null) {
-                try {
-                    ps.close();
-                } catch (SQLException e) {}
-            }
+    }
 
-            if (c != null) {
-                try {
-                    c.close();
-                } catch (SQLException e) {}
-            }
-        }
-
+    private static RowMapper<User> getUserRowMapper() {
+        return (rs, rowNum) -> {
+            User user = new User();
+            user.setId(rs.getString("id"));
+            user.setName(rs.getString("name"));
+            user.setPassword(rs.getString("password"));
+            return user;
+        };
     }
 }
--- src/test/java/springbook/user/dao/UserDaoTest.java
+++ src/test/java/springbook/user/dao/UserDaoTest.java
@@ -1,26 +1,18 @@
 package springbook.user.dao;
 
-import com.mysql.jdbc.Driver;
 import org.junit.Before;
 import org.junit.Test;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 import org.springframework.jdbc.datasource.SingleConnectionDataSource;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import springbook.user.domain.User;
 
 import javax.sql.DataSource;
 import java.sql.SQLException;
+import java.util.List;
 
-//@RunWith(SpringJUnit4ClassRunner.class)
-//@ContextConfiguration(locations = "UserDaoTest-context.xml")
 public class UserDaoTest {
 
     private UserDao dao;
@@ -29,7 +21,7 @@ public class UserDaoTest {
     private User user3;
 
     @Before
-    public void setUp() {
+    public void setUp() throws SQLException {
         this.user1 = new User("minpearl", "민휘", "lololo");
         this.user2 = new User("seeun", "세은", "030614");
         this.user3 = new User("kong", "콩이", "piggy");
@@ -37,9 +29,6 @@ public class UserDaoTest {
         this.dao = new UserDao();
         DataSource dataSource = new SingleConnectionDataSource("jdbc:mysql://localhost/toby", "root", "star0826", true);
         dao.setDataSource(dataSource);
-        JdbcContext jdbcContext = new JdbcContext();
-        jdbcContext.setDataSource(dataSource);
-        dao.setJdbcContext(jdbcContext);
     }
 
     @Test
@@ -58,6 +47,38 @@ public class UserDaoTest {
         User userget2 = dao.get(user2.getId());
         assertThat(userget2.getName(), is(user2.getName()));
         assertThat(userget2.getPassword(), is(user2.getPassword()));
+    }
+
+    @Test
+    public void getAll() throws SQLException {
+        dao.deleteAll();
+
+        List<User> users0 = dao.getAll();
+        assertThat(users0.size(), is(0));
+
+        dao.add(user1);
+        List<User> users1 = dao.getAll();
+        assertThat(users1.size(), is(1));
+        checkSameUser(user1, users1.get(0));
+
+        dao.add(user2);
+        List<User> users2 = dao.getAll();
+        assertThat(users2.size(), is(2));
+        checkSameUser(user1, users2.get(0));
+        checkSameUser(user2, users2.get(1));
+
+        dao.add(user3);
+        List<User> users3 = dao.getAll();
+        assertThat(users3.size(), is(3));
+        checkSameUser(user3, users3.get(0));
+        checkSameUser(user1, users3.get(1));
+        checkSameUser(user2, users3.get(2));
+    }
+
+    private void checkSameUser(User user1, User user2) {
+        assertThat(user1.getId(), is(user2.getId()));
+        assertThat(user1.getName(), is(user2.getName()));
+        assertThat(user1.getPassword(), is(user2.getPassword()));
     }
 
     @Test
